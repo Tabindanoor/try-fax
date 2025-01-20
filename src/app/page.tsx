a36a4556ecebc316676d389d8455a8a09f89b88b
Tabindanoor <tabindanoor415@gmail.com>
2025-01-20 19:09:10 +0500

    not able to deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-   <div>
-    i am gonna be try it after coming to my home in sha Allah
-   </div>
-  );
-}
+// export default function Home() {
+//   return (
+//    <div>
+//     i am gonna be try it after coming to my home in sha Allah
+//    </div>
+//   );
+// }

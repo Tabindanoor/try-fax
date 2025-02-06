6622ca38f07f10b2dcc94266a7e1634070f878f7
Tabindanoor <tabindanoor415@gmail.com>
2025-02-06 22:21:10 +0500

    not able to deploy

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const page =()=>{
+    return (
+        <div className="page">
+        <h1>My Fax</h1>
+        <input type="text" placeholder="Enter your message"/>
+        <button>Send</button>
+        </div>
+    )
+}
+
+export default page;

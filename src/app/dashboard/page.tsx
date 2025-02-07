1991accac8501a998f4ff01506d6fa1e36001931
Tabindanoor <tabindanoor415@gmail.com>
2025-02-07 22:51:49 +0500

    not able to deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,14 @@ const page =()=>{
         <h1>My Fax</h1>
         <input type="text" placeholder="Enter your message"/>
         <button>Send</button>
+        \begin{pre}
+        \end{pre>
+        </div>
+        <div className="page">
+            \begin{pre}
+            \end{pre>
+            <h1>My Fax</h1>
+            <input type="text" placeholder="Enter your message"/>
         </div>
     )
 }

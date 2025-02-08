2ac99b2f610f11bea3f6d796e71f66c7191cabfd
Tabindanoor <tabindanoor415@gmail.com>
2025-02-08 20:07:06 +0500

    not able to deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,9 @@
-const page =()=>{
-    return (
-        <div className="page">
-        <h1>My Fax</h1>
-        <input type="text" placeholder="Enter your message"/>
-        <button>Send</button>
-        \begin{pre}
-        \end{pre>
-        </div>
-        <div className="page">
-            \begin{pre}
-            \end{pre>
-            <h1>My Fax</h1>
-            <input type="text" placeholder="Enter your message"/>
-        </div>
-    )
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
-export default page;
+export default page
